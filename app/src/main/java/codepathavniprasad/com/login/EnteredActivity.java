33e69888ff78f4a2cf100055552af4c8ef71cc6c
Avni Prasad <avniprasad@fb.com>
2018-06-28 13:21:37 -0700

    Initial commit

---
+++ app/src/main/java/codepathavniprasad/com/login/EnteredActivity.java
@@ -0,0 +1,13 @@
+package codepathavniprasad.com.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnteredActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entered);
+    }
+}
---
+++ app/src/main/java/codepathavniprasad/com/login/LoginActivity.java
@@ -0,0 +1,22 @@
+package codepathavniprasad.com.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    public void onAddItem(View v) {
+        Intent i = new Intent(this, EnteredActivity.class);
+        startActivity(i);
+    }
+
+}
